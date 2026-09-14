bec703e3cda30077dcbb082a05d3fb88153f7c34
Gabriel Condori <hr.gabriel.29@gmail.com>
2026-09-14 19:25:30 -0400

    fix: corregir caso culpable, todos los checks en verde

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from '@playwright/test'
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './e2e',
+  testDir: "./e2e",
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: "http://localhost:5173",
   },
   webServer: [
+    // {
+    //   command: 'npm run dev --prefix ../backend',
+    //   url: 'http://localhost:3000', // Ajusta al puerto donde corre tu backend (ej. 3000 o 4000)
+    //   reuseExistingServer: !process.env.CI,
+    //   timeout: 120 * 1000,
+    // },
     {
-      command: 'npm run dev --prefix ../backend',
-      url: 'http://localhost:3000', // Ajusta al puerto donde corre tu backend (ej. 3000 o 4000)
-      reuseExistingServer: !process.env.CI,
-      timeout: 120 * 1000,
-    },
-    {
-      command: 'npm run dev',
-      url: 'http://localhost:5173',
+      command: "npm run dev",
+      url: "http://localhost:5173",
       reuseExistingServer: !process.env.CI,
       timeout: 120 * 1000,
     },
   ],
-})
+});
