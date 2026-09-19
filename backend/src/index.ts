cb9f7750e6b1c20f0c56fdf936dda15e60acf8a7
Gabriel Condori <hr.gabriel.29@gmail.com>
2026-09-19 00:55:50 -0400

    test: prueba error health

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
-
+throw new Error('fallo simulado') 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
